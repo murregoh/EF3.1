93a506fd2fa9d3f3753fce8140a767a03f34b60d
Mateo Urrego Hoyos <mateourregohoyos@Usuarios-MacBook-Pro.local>
2023-03-16 22:16:51 -0500

    addinig new relationships

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -8,10 +8,16 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
-
+        public DbSet<Battle> Battles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost;Database=SamuraiDB;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=tcp:muhserver.database.windows.net,1433;Initial Catalog=SamuraiDB;Persist Security Info=False;User ID=murregoh;Password=Dior0114;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
         }
     }
 }
